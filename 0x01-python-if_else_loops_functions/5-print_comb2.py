ac18b638116449ff048fd2d0672f30c5b533e0d6
ahmadoa <ouladaouida@gmail.com>
2023-05-09 03:29:23 -0700

    corrected output styling

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{:02d}".format(i), end="")
     if i != 99:
-        print(", ", end="")
-print()
+        print("{:02d}".format(i), end=", ")
+    elif i == 99:
+        print("{:02d}".format(i))
