69baf308f2b903b4bc714fd2deaebde18936fa48
ahmadoa <ouladaouida@gmail.com>
2023-05-09 06:24:19 -0700

    adding new node to existing singly linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a node in a singly linked list
+ * @head: already existing elements in linked list
+ * @number: number to add to the new node
+ * Return: new linked list
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *prev = NULL;
+	listint_t *curr = *head;
+	listint_t *temp_new;
+
+	*temp_new = malloc(sizeof(listint_t));
+	if (temp_new == NULL)
+		return (NULL);
+
+	temp_new->n = number;
+
+	while (curr && curr->n < number)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+
+	temp_new->next = curr;
+	if (prev)
+		prev->next = temp_new;
+	else
+		*head = temp_new;
+	return (temp_new);
+}
