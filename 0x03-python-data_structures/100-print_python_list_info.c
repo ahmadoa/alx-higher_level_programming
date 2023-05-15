8ebbccd2ffe0a4a08401c52bb60880c8ca9d2b5a
ahmadoa <ouladaouida@gmail.com>
2023-05-15 14:15:34 -0700

    updated loop in file 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,15 +4,13 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int i = 0;
+	int i;
 	long int length = PyList_Size(p);
 	PyListObject *elements = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", length);
 	printf("[*] Allocated = %li\n", elements->allocated);
-	while (i < length)
-	{
+
+	for (i = 0; i < size; i++)
 		print("Element %i: %s\n", i, Py_TYPE(elements->ob_item[i])->tp_name);
-		i++;
-	}
 }
