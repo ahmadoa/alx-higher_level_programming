109effe36aef725bf2f60c1115ea150500646082
ahmadoa <ouladaouida@gmail.com>
2023-05-17 11:26:18 -0700

    corrected mistake

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ def square_matrix_simple(matrix=[]):
     temp_list = []
     for i in range(0, len(matrix) - 1):
         for j in range(0, len(matrix) - 1):
-            temp_list.append(matrix[i][j]**2)
+            temp_list.append(matrix[i][j]*matrix[i][j])
         new_matrix.append(temp_list)
         temp_list = []
     return new_matrix
