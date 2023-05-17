f73cc70906e9cf287d1ee9a0c0eaff8d4bae34a7
ahmadoa <ouladaouida@gmail.com>
2023-05-17 12:13:20 -0700

    updated code so that the og dict to be mutated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    dict_copy = a_dictionary.copy()
-    dict_copy.update({key: value})
-    return dict_copy
+    a_dictionary.update({key: value})
+    return a_dictionary
