f56f457129f4f8fe1a6e11f0dd130c28e08b6a59
Ahmad Ouladaouid <84430036+ahmadoa@users.noreply.github.com>
2023-05-17 22:04:38 +0100

    Delete 9-multiple_by_2.py

--- 0x04-python-more_data_structures/9-multiple_by_2.py
+++
@@ -1,7 +0,0 @@
-#!/uar/bin/python3
-
-def multiply_by_2(a_dictionary):
-    multi_dict = {}
-    for key, val in a_dictionary.items():
-        multi_dict.update({key: val * 2})
-    return multi_dict
