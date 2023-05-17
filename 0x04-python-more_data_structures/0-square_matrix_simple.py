8b268bcd950c3c6ce5f8a62f51d3acf93fe12e5f
ahmadoa <ouladaouida@gmail.com>
2023-05-17 11:30:21 -0700

    updated algo

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    temp_list = []
-    for i in range(0, len(matrix) - 1):
-        for j in range(0, len(matrix) - 1):
-            temp_list.append(matrix[i][j]*matrix[i][j])
-        new_matrix.append(temp_list)
-        temp_list = []
-    return new_matrix
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
