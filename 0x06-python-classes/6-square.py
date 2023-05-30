d65ecc655a896222e4128872b1f0345dff7c02d6
ahmadoa <ouladaouida@gmail.com>
2023-05-30 02:33:46 -0700

    updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,5 +51,8 @@ class Square:
         for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+            for j in range(0, self.__position[0]):
+                print(" ", end="")
+            for k in range(0, self.__size):
+                print("#", end="")
+            print()
