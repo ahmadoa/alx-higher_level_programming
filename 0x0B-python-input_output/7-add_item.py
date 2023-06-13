ad8c2e921c70accb37cf67848f68ea669a183573
ahmadoa <ouladaouida@gmail.com>
2023-06-13 08:39:28 -0700

    script that adds args to a python list and saves the list to json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import json
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+file = "add_item.json"
+jsonl = []
+
+if os.path.exists(file):
+    jsonl = load_from_json_file(file)
+
+for i in range(1, len(sys.argv)):
+    jsonl.append(sys.argv[i])
+
+save_to_json_file(jsonl, file)
