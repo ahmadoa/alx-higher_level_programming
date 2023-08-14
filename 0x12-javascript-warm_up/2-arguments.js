244ab3d0eec3e598d371ffc151ac51a7c0adb667
ahmadoa <ouladaouida@gmail.com>
2023-08-14 02:21:09 -0700

    corrected format according to semistandard rules

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const numArgs = process.argv.length;
-console.log(numArgs === 2 ? "No argument" : numArgs === 3 ? "Argument found" : "Arguments found");
+console.log(numArgs === 2 ? 'No argument' : numArgs === 3 ? 'Argument found' : 'Arguments found');
